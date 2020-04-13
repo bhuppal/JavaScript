e2404dc090915ab0cc827f8441bdda2797ec50e2
Bhuppal Kumar G H <bhuppal@hotmail.com>
2020-04-12 20:39:12 -0400

    JSConcepts_13 file

---
+++ JSConcepts_13.js
@@ -0,0 +1,9 @@
+default export function myLibrary() {
+    console.log('Inside my library');
+}
+
+export function otherLibraryFunction() {
+    console.log('other library function');
+}
+
+//SSimport {myLibrary, otherLibraryFunction} from './JSConcepts_13';
