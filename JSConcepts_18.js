e25875eded115ba114ef5ac0ead9a75cb2085bce
Bhuppal Kumar G H <bhuppal@hotmail.com>
2020-04-13 12:11:56 -0400

    Add JSConcepts_18 file

---
+++ JSConcepts_18.js
@@ -0,0 +1,25 @@
+
+console.log(1);
+var myPromise = new Promise((resolve, reject) => {
+    // async code in here (usually)
+    console.log(2);
+    var noError = true;
+    if(noError) {
+        console.log(3);
+        resolve();        
+    }
+    else {
+        reject();
+    }
+});
+
+//do some stuff
+console.log(4);
+
+myPromise.then(() => {
+    console.log(5);
+    console.log('Sucess');        
+}).catch(() => {
+    console.log('Failure');    
+});
+
